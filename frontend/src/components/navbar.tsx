52898c4384d1f62d4c406bd77bb647fef504bba4
cassandra18 <cassandralelei013@gmail.com>
2025-03-19 15:55:08 +0300

    first commit

---
+++ frontend/src/components/navbar.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Navbar: React.FC = () => {
+    return (
+    <div className='bg-[#0C834E] flex justify-evenly'>
+        <div className=' '>
+            <ul>
+                <li>facebook</li>
+                <li>instagram</li>
+                <li>twitter</li>
+                <li>linkedin</li>
+            </ul>
+        </div>
+
+        <div>
+            <ul>
+                <li>0113120575</li>
+                <li>info@cbckericho.jn.ke</li>
+            </ul>
+        </div>
+    </div>
+    )
+}
+
+export default Navbar;
