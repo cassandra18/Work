21175905e5fe0e9fb1618abfb11bf63d28c58638
cassandra18 <cassandralelei013@gmail.com>
2025-04-23 10:11:50 +0300

    added scrolltotop component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,14 @@ import BackToTop from "./components/backToTop"
 import Footer from "./components/footer"
 import Navbar from "./components/navbar"
 import { Outlet } from 'react-router-dom'
+import ScrollToTop from "./components/scrollToTop"
+
 
 function App() {
 
   return (
     <div className="bg-gradient-to-br from-green-50 to-white min-h-screen">
+      <ScrollToTop />
       <Navbar />
       <Outlet />
       <Footer />
